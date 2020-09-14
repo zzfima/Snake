eab5ba105436f54dbbf03406d83ce7aecc259567
Efim Zabarsky <zzfima@gmail.com>
2020-09-14 06:17:49 +0300

    rfct

--- settings/constants.py
+++ settings/constants.py
@@ -2,9 +2,13 @@
 
 import numpy as np
 
-SIZE = (32, 32) # size of the grid world
-SNAKE_SIZE = 3 # initial size of the snake
-# numeric representation for different types of objects 
+# size of the grid world
+SIZE = (32, 32)
+
+# initial size of the snake
+SNAKE_SIZE = 3
+
+# numeric representation for different types of objects
 WALL = 255
 FOOD_BLOCK = 64
 SNAKE_BLOCK = 100
@@ -19,6 +23,7 @@ DIRECTIONS = [np.array([-1, 0]),
               np.array([0, 1]),
               np.array([1, 0]),
               np.array([0, -1])]
+
 # rewards for different events
 DEAD_REWARD = -1.0
 MOVE_REWARD = 0.0
