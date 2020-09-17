8e5aeaeaaf35d6cfba566d27275773d66b3bdf80
Efim Zabarsky <zzfima@gmail.com>
2020-09-17 06:48:45 +0300

    continue

--- env/core/snake.py
+++ env/core/snake.py
@@ -1,15 +1,43 @@
-# snake properties
-
 import numpy as np
 
 from settings.constants import DIRECTIONS, SNAKE_BLOCK
 
 
 class Snake:
-    def __init__(self, param1, param2, ...):
+    def __init__(self, head_position, direction_index, length):
+        """
+        @param head_position (x, y): tuple
+        @param direction_index (0-3 (u, r, d, l)): int
+        @param length: int
+        """
         # Information snake need to know to make the move
-        pass
+        self.snake_block = SNAKE_BLOCK  # snake identification?
+        self.current_direction_index = direction_index  # 0-3 (u, r, d, l)
+
+        self.alive = True # Alive identifier
+
+        # Place the snake
+        self.blocks = [head_position]
+        current_position = np.array(head_position)
+        for i in range(1, length):
+            # Direction inverse of moving
+            current_position = current_position - DIRECTIONS[self.current_direction_index]
+            self.blocks.append(tuple(current_position))
 
     def step(self, action):
         # Execute one-time step within the environment
-        pass
+        """
+        @param action: int
+        @param return: tuple, tuple
+        """
+        # Check if action can be performed (do nothing if in the same direction or opposite)
+        if (self.alive) and ():
+            self.current_direction_index = action
+        # Remove tail
+        tail =
+        self.blocks =
+        # Check new head
+        new_head =
+        # Add new head
+        self.blocks = [new_head] + self.blocks
+        return new_head, tail
