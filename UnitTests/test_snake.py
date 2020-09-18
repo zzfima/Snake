6ebf06a70ca63170a3279da86e4f3e8d7a091aad
Efim Zabarsky <zzfima@gmail.com>
2020-09-18 07:57:56 +0300

    test

--- UnitTests/test_snake.py
+++ UnitTests/test_snake.py
@@ -1,5 +1,8 @@
 from env.core.snake import Snake
 
 def test_init():
-    snake =  Snake()
+    head_position = (3,5)
+    direction_index = 2
+    length = 4
+    snake =  Snake(head_position, direction_index, length)
     assert snake != None
